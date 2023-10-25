e570aeeb0dcd47b98c89a704805bc1a7787304d8
gijohardent <s2200152@unklab.ac.id>
2023-10-25 12:02:30 +0800

    update exercise10

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState,useEffect } from 'react'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Footer from './Components/Footer'
 
 export default function App() {
   
-  const [name, setName] = useState([]);
-
-  useEffect(() => {
-    names()
-  }, [])
-
-  const names = async () => {
-    const responce = await fetch('https://reqres.in/api/users')
-    setName(await responce.json())
-  }
-
   return (
     <>
-      <div key={data.id}>
-        <h1>Nama-nama</h1>
-      </div>
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
